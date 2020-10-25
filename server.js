87bd672f01ff05ed20a13452bb47d18cbc739ac0
kakeng <kakeng@easystore.co>
2020-10-25 20:01:38 +0800

    removed session

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const session = require('express-session')
 const router  = require('./routers')
 
 const app  = express()
@@ -14,16 +13,6 @@ app.use(
 
 app.use(express.json())
 app.use(cors())
-
-app.set('trust proxy', 1)
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 365 * 1000
-  },
-  saveUninitialized: true
-}))
 
 app.use('/', router)
 
