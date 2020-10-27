2a97525c4d80bd9a02da56d5ed83c08a8561c649
kakeng <kakeng@easystore.co>
2020-10-27 18:11:45 +0800

    ip

--- middlewares/get-client-info.js
+++ middlewares/get-client-info.js
@@ -3,26 +3,26 @@ const GeoIp = require('geoip-lite')
 
 module.exports = async (req, res, next) => {
 
-    // req.ip = RequestIp.getClientIp(req)
+    const ip = RequestIp.getClientIp(req)
 
     // let ip = '60.52.31.157'
 
-    if(req.body.ip) {
+    // if(req.body.ip) {
 
-        req.ip = req.body.ip
+    //     req.ip = req.body.ip
 
-    } else {
+    // } else {
 
-        req.ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-        req.connection.remoteAddress || 
-        req.socket.remoteAddress || 
-        (req.connection.socket ? req.connection.socket.remoteAddress : '127.0.0.1')
+    //     req.ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+    //     req.connection.remoteAddress || 
+    //     req.socket.remoteAddress || 
+    //     (req.connection.socket ? req.connection.socket.remoteAddress : '127.0.0.1')
 
-    }
+    // }
 
-    console.log(req.ip)
+    console.log(ip)
     
-    req.geo = GeoIp.lookup(req.ip) || { city: '', country: '' }
+    req.geo = GeoIp.lookup(ip) || { city: '', country: '' }
 
     next()
 
