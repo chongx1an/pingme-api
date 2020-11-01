b14de266027de2288c42812815fb2e3edae6e6d9
kakeng <kakeng@easystore.co>
2020-11-01 12:02:16 +0800

    enhance error handler

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -1,17 +1,55 @@
 const { WebClient } = require('@slack/web-api')
 const Slack = new WebClient(process.env.SLACK_TOKEN)
 
-process.on('uncaughtException', function (err) {
-    Slack.chat.postMessage({
+process.on('uncaughtException', async function (err) {
+
+    let errStack = err.stack.split('\n')
+    const firstLine = errStack.shift()
+
+    errStack = [`*${firstLine}*\n`, ...errStack].join('\n')
+
+    const blocks = [
+        {
+            type: 'header',
+            text: {
+                type: 'plain_text',
+                text: 'API',
+            }
+        },
+        {
+			type: 'divider'
+        },
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: errStack,
+            }
+        }
+    ]
+
+    await Slack.chat.postMessage({
         channel: '#errors',
-        text: JSON.stringify(err.stack)
+        blocks,
     })
+
 })
 
-process.on('unhandledRejection', function(reason, p){
-    Slack.chat.postMessage({
-        channel: '#errors',
+process.on('unhandledRejection', async function(reason, promise) {
 
-        text: JSON.stringify(reason)
+    const blocks = [
+        {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: reason,
+            },
+        }
+    ]
+
+    await Slack.chat.postMessage({
+        channel: '#errors',
+        blocks,
     })
+
 })
