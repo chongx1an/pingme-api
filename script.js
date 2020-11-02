896e9a593e64310ad26cd6b0d42022cd0d8c8fa9
kakeng <kakeng@easystore.co>
2020-11-02 15:49:27 +0800

    removed duration

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-const { base } = require("./models/team")
-
 const customerId = __st ? __st.cid : null
 
 if(customerId) {
 
-  const enterTime = new Date()
+    const template = location.pathname.split('/').reverse()[1]
 
-  const baseUrl = 'https://the-pingme-api.herokuapp.com/shopify'
-
-  const template = location.pathname.split('/').reverse()[1]
-
-  window.onunload = function() {
-
-    const leaveTime = new Date()
-
-    const duration = (leaveTime - enterTime) / 1000
-    
     if(template == 'products' || template == 'collections') {
 
-      const handle = location.pathname.split('/').pop()
+        const baseUrl = 'https://the-pingme-api.herokuapp.com/shopify/view'
+        const handle = location.pathname.split('/').pop()
 
-      $.ajaxSetup({ async: false })
-
-      if(template == 'products') {
-        
-        $.getJSON(`/products/${handle}.json`, res => {
-          $.get(`${baseUrl}/view/products/${res.product.id}?shop=${Shopify.shop}&customerId=${customerId}&duration=${duration}`)
-        })
+        if(template == 'products') {
           
-      } else if(template == 'collections') {
-        
-        $.getJSON(`/collections/${handle}.json`, res => {
-          $.get(`${baseUrl}/view/collections/${res.collection.id}?shop=${Shopify.shop}&customerId=${customerId}&duration=${duration}`)
-        })
+          $.getJSON(`/products/${handle}.json`, res => {
+            $.get(`${baseUrl}/products/${res.product.id}?shop=${Shopify.shop}&customerId=${customerId}`)
+          })
+            
+        } else if(template == 'collections') {
+          
+          $.getJSON(`/collections/${handle}.json`, res => {
+            $.get(`${baseUrl}/collections/${res.collection.id}?shop=${Shopify.shop}&customerId=${customerId}`)
+          })
 
-      }
+        }
 
     }
-
-  }
 
 }
