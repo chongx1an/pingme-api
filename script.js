6af0631d5f1b19469858792135a4138399a1d262
kakeng <kakeng@easystore.co>
2020-11-03 19:58:30 +0800

    fix

--- script.js
+++ script.js
@@ -4,24 +4,13 @@ if(customerId) {
 
     const template = location.pathname.split('/').reverse()[1]
 
-    if(template == 'products' || template == 'collections') {
+    if(['products', 'collections'].includes(template)) {
 
-        const baseUrl = 'https://the-pingme-api.herokuapp.com/shopify/view'
         const handle = location.pathname.split('/').pop()
 
-        if(template == 'products') {
-          
-          $.getJSON(`/products/${handle}.json`, res => {
-            $.get(`${baseUrl}/products/${res.product.id}?shop=${Shopify.shop}&customerId=${customerId}`)
-          })
-            
-        } else if(template == 'collections') {
-          
-          $.getJSON(`/collections/${handle}.json`, res => {
-            $.get(`${baseUrl}/collections/${res.collection.id}?shop=${Shopify.shop}&customerId=${customerId}`)
-          })
-
-        }
+        $.getJSON(`/${template}/${handle}.json`, res => {
+          $.get(`https://the-pingme-api.herokuapp.com/shopify/view/${template}/${res[template].id}?shop=${Shopify.shop}&customerId=${customerId}`)
+        })
 
     }
 
