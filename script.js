616f30933d59d1358d7582a93455d7c01c45f7c4
kakeng <kakeng@easystore.co>
2020-11-06 13:22:04 +0800

    script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const customerId = __st ? __st.cid : null
+
 const template = location.pathname.split('/').reverse()[1]
 
 const apiUrl = 'https://the-pingme-api.herokuapp.com/shopify'
@@ -12,7 +14,7 @@ const isSpam = () => {
 
 }
 
-if(__st.cid && !isSpam()) {
+if(customerId && !isSpam()) {
 
   // localStorage.clear()
   // localStorage.setItem(location.pathname, Date.now())
@@ -24,7 +26,7 @@ if(__st.cid && !isSpam()) {
     case '':
 
       // View home event
-      $.get(`${apiUrl}/home/view?shop=${Shopify.shop}&customerId=${__st.cid}`)
+      $.get(`${apiUrl}/home/view?shop=${Shopify.shop}&customerId=${customerId}`)
       
     break
 
@@ -35,12 +37,12 @@ if(__st.cid && !isSpam()) {
       $.getJSON(`/products/${handle}.json`).then(res => {
 
         // View product event
-        $.get(`${apiUrl}/products/${res.product.id}/view?shop=${Shopify.shop}&customerId=${__st.cid}`)
+        $.get(`${apiUrl}/products/${res.product.id}/view?shop=${Shopify.shop}&customerId=${customerId}`)
 
         // Add to cart event
         document.getElementsByClassName('btn product-form__cart-submit')[0].addEventListener('click', function() {
           
-          $.get(`${apiUrl}/products/${res.product.id}/cart?shop=${Shopify.shop}&customerId=${__st.cid}`)
+          $.get(`${apiUrl}/products/${res.product.id}/cart?shop=${Shopify.shop}&customerId=${customerId}`)
 
         })
 
@@ -54,7 +56,7 @@ if(__st.cid && !isSpam()) {
 
       // View collection event
       $.getJSON(`/${template}/${handle}.json`, res => {
-        $.get(`${apiUrl}/collections/${res.collection.id}/view?shop=${Shopify.shop}&customerId=${__st.cid}`)
+        $.get(`${apiUrl}/collections/${res.collection.id}/view?shop=${Shopify.shop}&customerId=${customerId}`)
       })
 
     break
